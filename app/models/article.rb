6f57f41045875cbb1ac9e37dcbc458de04922525
Robbie Lane <lane.robbie@gmail.com>
2015-10-27 23:32:37 -0600

    Tagging functionality working

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,15 @@
 class Article < ActiveRecord::Base
+  has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(', ')
+  end
+
+  def tag_list=(tags_string)
+    tags = tags_string.split(',').collect { |s| s.strip.downcase }.uniq
+    new_or_found_tags = tags.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
